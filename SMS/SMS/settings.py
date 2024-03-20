116e10db7bd6f8fcc438bde202752c5964fa0043
Shivamkrchaudhary <shivamkrchaudhary141@gmail.com>
2024-03-20 10:49:09 +0530

    Finally Login Page Created

--- SMS/SMS/settings.py
+++ SMS/SMS/settings.py
@@ -116,10 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"                                             # Manually Added By Shivam
+    BASE_DIR / "/manager/static",                                               # Manually Added By Shivam
 ]
 
 # Default primary key field type
--- SMS/manager/views.py
+++ SMS/manager/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def login(request):
-    return render(request,'login.html')
+    return render(request, 'login.html')
